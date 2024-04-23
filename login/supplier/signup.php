84d25f658cb7c0db39f72bac672461059addfed5
unknown <ankitbkana@outlook.com>
2024-04-23 22:14:44 +0530

    form fields are validate

--- login/supplier/signup.php
+++ login/supplier/signup.php
@@ -3,6 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer(true);
+$emailSend = false;
 
 session_start(); // Start the session
 
@@ -15,8 +16,9 @@ function generateOTP()
 $step1 = true;
 $step2 = false;
 
-if (isset($_POST['userEmail']))
+if (isset($_POST['userEmail']) && isset($_POST['Next']))
 {
+    echo $_POST['Next'];
     $userEmail = $_POST['userEmail'];
     $_SESSION['userEmail'] = $userEmail;
     // Generate and store OTP in session
@@ -40,18 +42,11 @@ if (isset($_POST['userEmail']))
     try
     {
         $mail->send();
-        echo `<script>
-                const emailToast = document.getElementById('emailToast')
-                const emailToastOK = bootstrap.Toast.getOrCreateInstance(emailToast)
-                emailToastOK.show() 
-                </script>`;
+        $emailSend = true;
     } catch (Exception $e)
     {
         echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
     }
-
-
-
     $step1 = false;
     $step2 = true;
 }
@@ -59,15 +54,19 @@ if (isset($_POST['userEmail']))
 
 
 
-<form class="shadow p-3 rounded" method="POST" action="">
+<form class="shadow p-3 rounded needs-validation" novalidate method="POST" action="">
     <div class="text-center mb-3">
         <h4>Sign Up</h4>
     </div>
     <div class="mb-3" <?php echo $step1 ? '' : 'hidden' ?>>
         <label for="userEmail" class="form-label">Email address</label>
         <input autocomplete="off" type="email" class="form-control" id="userEmail" name="userEmail"
-            aria-describedby="emailHelp" value="<?php echo isset($_POST['userEmail']) ? $_POST['userEmail'] : '' ?>">
+            aria-describedby="emailHelp" value="<?php echo isset($_POST['userEmail']) ? $_POST['userEmail'] : '' ?>"
+            required>
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <div class="invalid-feedback">
+            Please enter a valid email.
+        </div>
     </div>
     <div class="my-2 text-muted fs-6" <?php echo $step2 ? '' : 'hidden' ?>>
         <p class="fs-6">Check OTP on <span
@@ -75,15 +74,15 @@ if (isset($_POST['userEmail']))
     </div>
     <div class="mb-3" <?php echo $step2 ? '' : 'hidden' ?>>
         <label for="userPassword" class="form-label">Password</label>
-        <input type="password" class="form-control" id="userPassword" name="userPassword">
+        <input type="password" class="form-control" id="userPassword" name="userPassword" <?php echo $step2 ? 'required' : 'hidden' ?>>
     </div>
     <div class="mb-3" <?php echo $step2 ? '' : 'hidden' ?>>
         <label for="userConfirmPassword" class="form-label">Confirm Password</label>
-        <input type="password" class="form-control" id="userConfirmPassword" name="userConfirmPassword">
+        <input type="password" class="form-control" id="userConfirmPassword" name="userConfirmPassword" <?php echo $step2 ? 'required' : 'hidden' ?>>
     </div>
     <div class="mb-3" <?php echo $step2 ? '' : 'hidden' ?>>
         <label for="userOtp" class="form-label">OTP</label>
-        <input type="number" class="form-control" id="userOtp" name="userOtp">
+        <input type="number" class="form-control" minlength="6" maxlength="6" id="userOtp" name="userOtp" <?php echo $step2 ? 'required' : 'hidden' ?>>
     </div>
     <div class="mb-3 form-check" hidden>
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -91,7 +90,36 @@ if (isset($_POST['userEmail']))
     </div>
 
     <div class="text-center">
-        <button type="submit" name="<?php echo $step1 ? 'Next ->' : 'Signup' ?>" class="btn btn-success text-center"
+        <button type="submit" name="<?php echo $step1 ? 'Next' : 'Signup' ?>" class="btn btn-success text-center"
             fdprocessedid="vw48xs" style="width: 100%;"><?php echo $step1 ? 'Next ->' : 'Signup' ?></button>
     </div>
 </form>
+<?php
+if ($emailSend)
+    echo `<script>
+                const emailToast = document.getElementById('emailToast')
+                const emailToastOK = bootstrap.Toast.getOrCreateInstance(emailToast)
+                emailToastOK.show() 
+                </script>`;
+?>
+
+<script>
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+</script>
