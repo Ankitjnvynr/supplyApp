0dafea4fa6dd07b2d1b115c4261ad88997f0644e
ankitjnvynr <ankitbkana@outlook.com>
2024-04-26 17:12:35 +0530

    try header creation

---
+++ partials/_header.php
@@ -0,0 +1,20 @@
+<style>
+    .search-input{
+        background: transparent;
+        outline: none;
+    }
+</style>
+
+<div class="container pt-3">
+    <div class="d-flex justify-content-between ">
+        <h3 class="text-success">ABC Supplier</h3>
+        <div class="d-flex gap-3 fs-3">
+            <a href="#"><i class="fa-solid fa-camera text-muted"></i></a>
+            <a href="#"><i class="fa-solid fa-ellipsis-vertical text-muted"></i></a>
+        </div>
+    </div>
+    <div class="rounded-pill bg-secondary-subtle p-2 px-3 d-flex align-items-center mb-2">
+        <i class="fa-solid fa-magnifying-glass fs-5"></i>
+        <input class="search-input m-0 p-0 form-control bg-none" type="text">
+    </div >
+</div>
