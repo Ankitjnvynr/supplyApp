1f74a15dcff8bf0dbc2298cebe5dd4539491c34a
unknown <ankitbkana@outlook.com>
2024-05-13 07:53:27 +0530

    try emailjs

--- login/shopee/signup.php
+++ login/shopee/signup.php
@@ -1,3 +1,8 @@
+
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+</script>    
+
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -21,79 +26,116 @@ $userOtpMsg = false;
 $userPwdMsg = false;
 $emailSend;
 
-if (isset($_POST['userEmail']) && isset($_POST['Next'])) {
+if (isset($_POST['userEmail']) && isset($_POST['Next']))
+{
     $userEmail = $_POST['userEmail'];
     $_SESSION['userEmail'] = $userEmail;
     // Generate and store OTP in session
     $_SESSION['otp'] = generateOTP();
     $sql = "SELECT email FROM `users` WHERE email = '$userEmail' ";
     $res = $conn->query($sql);
-    if ($res) {
+    if ($res)
+    {
         $nums = $res->num_rows;
-        if ($nums > 0) {
+        if ($nums > 0)
+        {
             $alreadyMsg = "Email already Exist";
-        } else {
-            $mail->isSMTP();
-            $mail->Host = 'smtp-mail.outlook.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'ankitbkana@outlook.com';
-            $mail->Password = 'ankit1558';
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
+        } else
+        {
+            // $mail->isSMTP();
+            // $mail->Host = 'smtp-mail.outlook.com';
+            // $mail->SMTPAuth = true;
+            // $mail->Username = 'ankitbkana@outlook.com';
+            // $mail->Password = 'ankit1558';
+            // $mail->SMTPSecure = 'tls';
+            // $mail->Port = 587;
 
-            $mail->setFrom('ankitbkana@outlook.com', 'Delever Goods');
-            $mail->addAddress($_SESSION['userEmail'], 'Recipient Name');
+            // $mail->setFrom('ankitbkana@outlook.com', 'Delever Goods');
+            // $mail->addAddress($_SESSION['userEmail'], 'Recipient Name');
 
-            $mail->Subject = 'OTP Varification';
-            $mail->Body = 'Your 6 digit varification code is ' . $_SESSION['otp'];
+            // $mail->Subject = 'OTP Varification';
+            // $mail->Body = 'Your 6 digit varification code is ' . $_SESSION['otp'];
 
-            //sendig email by try catch 
-            try {
-                $mail->send();
-                $emailSend = 'OTP sent to <span class="text-success">' . $_SESSION['userEmail'] . '</span>';
-            } catch (Exception $e) {
-                echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-            }
+            // //sendig email by try catch 
+            // try {
+            //     $mail->send();
+            //     $emailSend = 'OTP sent to <span class="text-success">' . $_SESSION['userEmail'] . '</span>';
+            // } catch (Exception $e) {
+            //     echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+            // }
+            $emailAdd = $_SESSION['userEmail'];
+            $otp = $_SESSION['otp'];
+
+            echo '
+            <script>
+            emailjs.init("y6h-t_BnDBEgh4v-k");
+            document.addEventListener("DOMContentLoaded", (event) => {
+            console.log("DOM fully loaded and parsed");
+
+                    emailjs.send("service_0hj770c", "template_n3ebbni", {
+                        to: "' . $emailAdd . '",
+                        from: "ankitbkana@outlook.com",
+                        subject: "Varification Code",
+                        text: "OTP",
+                        otp: "' . $otp . '"
+                    })
+                        .then(function (response) {
+                            console.log("Email sent successfully", response);
+                        }, function (error) {
+                            console.error("Email sending failed", error);
+                        });
+                        });
+            </script>
+            ';
             $step1 = false;
             $step2 = true;
         }
     }
 }
 
-if (isset($_POST['userPassword']) && isset($_POST['userConfirmPassword']) && isset($_POST['userOtp']) && isset($_POST['Signup'])) {
+if (isset($_POST['userPassword']) && isset($_POST['userConfirmPassword']) && isset($_POST['userOtp']) && isset($_POST['Signup']))
+{
     // $userEmail = $_POST['userEmail'];
     $userPassword = $_POST['userPassword'];
     $userConfirmPassword = $_POST['userConfirmPassword'];
     $userOtp = $_POST['userOtp'];
 
-    if ($userOtp == $_SESSION['otp'] && isset($_POST['userPassword'])) {
-        if ($userPassword == $userConfirmPassword) {
+    if ($userOtp == $_SESSION['otp'] && isset($_POST['userPassword']))
+    {
+        if ($userPassword == $userConfirmPassword)
+        {
             $userType = 'shopee';
             $sql = "INSERT INTO users (email, password,user_type) VALUES (?, ?,?)";
             // Prepare the statement
             $stmt = $conn->prepare($sql);
-            if ($stmt) {
+            if ($stmt)
+            {
                 // Bind parameters and execute the statement
                 $stmt->bind_param("sss", $_SESSION['userEmail'], md5($userPassword), $userType);
-                if ($stmt->execute()) {
+                if ($stmt->execute())
+                {
                     $_SESSION['loggedin'] = true;
                     // Redirect to step 2
                     header("Location: meta.php");
                     exit();
-                } else {
+                } else
+                {
                     echo "Error inserting record: " . $stmt->error;
                 }
                 // Close the statement
                 $stmt->close();
-            } else {
+            } else
+            {
                 echo "Error preparing statement: " . $conn->error;
             }
-        } else {
+        } else
+        {
             $userPwdMsg = '<span class="text-danger">Passwords not match';
             $step1 = false;
             $step2 = true;
         }
-    } else {
+    } else
+    {
         $userOtpMsg = '<span class="text-danger">Wrong! OTP. Check again';
         $step1 = false;
         $step2 = true;
@@ -107,11 +149,14 @@ if (isset($_POST['userPassword']) && isset($_POST['userConfirmPassword']) && iss
     </div>
     <div class="mb-3" <?php echo $step1 ? '' : 'hidden' ?>>
         <label for="userEmail" class="form-label">Email address</label>
-        <input autocomplete="false" aria-autocomplete="false" type="email" class="form-control" id="userEmail" name="userEmail" aria-describedby="emailHelp" value="<?php echo isset($_POST['userEmail']) ? $_POST['userEmail'] : '' ?>" required>
+        <input autocomplete="false" aria-autocomplete="false" type="email" class="form-control" id="userEmail"
+            name="userEmail" aria-describedby="emailHelp"
+            value="<?php echo isset($_POST['userEmail']) ? $_POST['userEmail'] : '' ?>" required>
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         <div class="text-danger fs-6">
             <?php
-            if (isset($alreadyMsg)) {
+            if (isset($alreadyMsg))
+            {
                 echo $alreadyMsg;
             }
             ?>
@@ -127,7 +172,8 @@ if (isset($_POST['userPassword']) && isset($_POST['userConfirmPassword']) && iss
     </div>
     <div class="mb-3" <?php echo $step2 ? '' : 'hidden' ?>>
         <label for="userPassword" class="form-label">Password</label>
-        <input autocomplete="off" aria-autocomplete="false" type="password" value="" class="form-control" id="userPassword" name="userPassword" <?php echo $step2 ? 'required' : 'hidden' ?>>
+        <input autocomplete="off" aria-autocomplete="false" type="password" value="" class="form-control"
+            id="userPassword" name="userPassword" <?php echo $step2 ? 'required' : 'hidden' ?>>
     </div>
     <div class="mb-3" <?php echo $step2 ? '' : 'hidden' ?>>
         <label for="userConfirmPassword" class="form-label">Confirm Password</label>
@@ -145,7 +191,8 @@ if (isset($_POST['userPassword']) && isset($_POST['userConfirmPassword']) && iss
     </div>
 
     <div class="text-center">
-        <button id="" type="submit" name="<?php echo $step1 ? 'Next' : 'Signup' ?>" class="btn btn-success text-center" fdprocessedid="vw48xs" style="width: 100%;"><?php echo $step1 ? 'Next ->' : 'Signup' ?></button>
+        <button id="user<?php echo $step1 ? 'Next' : 'Signup' ?>" type="submit" name="<?php echo $step1 ? 'Next' : 'Signup' ?>" class="btn btn-success text-center"
+            fdprocessedid="vw48xs" style="width: 100%;"><?php echo $step1 ? 'Next ->' : 'Signup' ?></button>
     </div>
     <p class="m-0 mt-3">Already an Account ? <a class="text-success fw-bold p-0 m-0" href="../supplier/">LogIn </a></p>
 </form>
