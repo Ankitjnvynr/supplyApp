0183c375bddc6a6a639b02776cbd036377a2ebac
unknown <ankitbkana@outlook.com>
2024-05-16 08:19:15 +0530

    try signup

--- login/shopee/checkExist.php
+++ login/shopee/checkExist.php
@@ -2,14 +2,15 @@
 require_once '../../partials/_db.php';
 $email = $_POST['userEmail'];
 // Function to check if an email exists
-
+$_SESSION['userEmail'] = $email;
 $sql = "SELECT * FROM users WHERE email = '$email'";
 $res = $conn->query($sql);
 $nums = $res->num_rows;
-if($nums>0){
+if ($nums > 0)
+{
     echo '1';
-}else{
+} else
+{
     echo '0';
 }
 
-  
